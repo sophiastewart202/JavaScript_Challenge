af3ba35f7883c64c3f1667c7dcc79609d2ca982c
Sophia <sophia.stewart202@protonmail.com>
2021-09-11 14:25:13 -0400

    finished code for charts that updates when different subject id is selected.

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -1,106 +1,119 @@
 // Use the D3 library to read in samples.json.
+buildCharts(940);
 
-d3.json("samples.json").then(data => {
-    // store data
-    let bbdata = data.samples;
-    console.log(bbdata);
+function buildCharts(selected_id) {
 
-    function init() {
-        let sample_values = bbdata[0].sample_values.slice(0,10).reverse();
-        let otu_ids = bbdata[0].otu_ids.slice(0,10).reverse();
-        let otu_labels = bbdata[0].otu_labels.slice(0,10).reverse();
+    d3.json("samples.json").then(data => {
+        // store data
+        console.log(data);
 
-        // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-        let trace1 = {
-            // Use sample_values as the values for the bar chart.
-            x: sample_values,
-            // Use otu_ids as the labels for the bar chart.
+        let ids = data.names;
+        console.log(ids);
 
-            // how do I get the number to stay the same when I convert it to a string?
-            y: otu_ids.toString(),
-            type: 'bar',
-            orientation: 'h',
-            // Use otu_labels as the hovertext for the chart.
-            hovertext: otu_labels,
-            connectgaps: true
-        };
+        // POPULATE DROPDOWN MENU
+        let dropdown = d3.select("#selDataset");
+        ids.forEach(id => {
+            dropdown.append('option').text(id)
+        });
+
+        let subjectData = selectData(selected_id, data);
+        let metadata = subjectData.metadata;
+        let samples = subjectData.samples;
+
+        init_meta(metadata);
+        init_bar(samples);
+        init_bubble(samples);
+    })
+};
+
+// function to reload page when new id is selected
+function optionChanged(id) {
+    console.log(id);
+    buildCharts(id);
+}
+
+// Filter data by subject id
+function selectData(id, data) {
+    let metadata = data.metadata.filter(subject => subject.id == id)[0];
+    let samples = data.samples.filter(sample => sample.id == id)[0];
+
+    return {
+        metadata: metadata,
+        samples: samples
+    };
+}
+
+// DEMOGRAPHIC INFO
+function init_meta(metadata) {
+    // select html tag
+    let target = d3.select("#sample-metadata");
+
+    // clear out contents to make way for new data
+    target.html("");
+
+    Object.entries(metadata).forEach(([key, value]) => 
+        {target.append("p").html(`<strong>${key}:</strong> ${value}`)
+    });
+};
+
+ // HORIZONTAL BAR CHART
+ function init_bar(samples) {
+    let sample_values = samples.sample_values.slice(0,10).reverse();
+    let otu_ids = samples.otu_ids;
+    let otu_labels = samples.otu_labels.slice(0,10).reverse();
+    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     
-        let Data = [trace1];
-        let layout = {
-            title: "Top 10 OTUs (Operational Taxonomic Units)",
-            yaxis: {
-                // ticks: labels(),
-                nticks: 10,
-                tickprefix: 'OTU '
-            }
-        };
-
-        Plotly.newPlot("bar", Data, layout);
-    }
-
-    // d3.selectAll("#selDataset").on("change", getData);
+    let trace1 = {
+        // Use sample_values as the values for the bar chart.
+        x: sample_values,
+        // Use otu_ids as the labels for the bar chart.
+        y: yticks,
+        type: 'bar',
+        orientation: 'h',
+        // Use otu_labels as the hovertext for the chart.
+        hovertext: otu_labels
+    };
     
-    // function getData() {
-    //     let dropdownMenu = d3.select("#selDataset");
-    //     // Assign the value of the dropdown menu option to a variable
-    //     let dataset = dropdownMenu.property("onchange");
-    //     // Initialize an empty array for the country's data
-    //     let data_list = []
-    //     let sample_values = [];
-    //     let otu_ids = []
-    //     let otu_labels = []
-        
-    //     for (let i=0; i < bbdata.length; i++) {
-    //         if (dataset == bbdata[i].id) {
-    //             sample_values = bbdata[i].sample_values.slice(0,10);
-    //             otu_ids = bbdata[i].otu_ids;
-    //             otu_labels = bbdata[i].otu_labels;
-
-    //             data_list.push(sample_values, otu_ids, otu_labels);
-    //         }
-    //     }
-
-        // Call function to update the chart
-    //     updatePlotly(data_list);
-    //     }
-    // }
-
-    // Update the restyled plot's values
-    // function updatePlotly(newdata) {
-    //     Plotly.restyle("bar", "y", [newdata.sample_values]);
-    //   }
-    // Create a bubble chart that displays each sample.
-    function init_bubble() {
-        let sample_values = bbdata[0].sample_values;
-        let otu_ids = bbdata[0].otu_ids;
-        let otu_labels = bbdata[0].otu_labels;
-
-        let trace2 = {
-            x: otu_ids,
-            y: sample_values,
-            mode: 'markers',
-            text: otu_labels,
-            marker: {
-                size: sample_values,
-                color: otu_ids
-            },
-            type: 'bubble'
-        };
-      
-        let Data = [trace2];
-      
-        let layout = {
-            title: 'OTU Samples',
-            showlegend: false,
-            xaxis: {
-                title: 'OTU ID'
-            }
-        };
-      
-        Plotly.newPlot('bubble', Data, layout);
-
+    let Data = [trace1];
+    let layout = {
+        title: "Top 10 OTUs (Operational Taxonomic Units)",
+        yaxis: {
+            nticks: 10
+        }
     };
 
-    init();
-    init_bubble();
-});
+    Plotly.newPlot("bar", Data, layout);
+}
+
+
+// BUBBLE CHART
+function init_bubble(samples) {
+    let sample_values = samples.sample_values;
+    let otu_ids = samples.otu_ids;
+    let otu_labels = samples.otu_labels;
+
+    let trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        mode: 'markers',
+        text: otu_labels,
+        marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: 'YlGnBu'
+        },
+        type: 'bubble'
+    };
+  
+    let Data = [trace2];
+  
+    let layout = {
+        title: 'OTU Samples',
+        showlegend: false,
+        xaxis: {
+            title: 'OTU ID'
+        }
+    };
+  
+    Plotly.newPlot('bubble', Data, layout);
+};
